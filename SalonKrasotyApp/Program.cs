0a6d2ce19e933191b8c957e728a12f7c47f48ef3
Sheltiq <sheltiqq@gmail.com>
2025-05-11 15:23:47 +0500

    added check of database connection and resources for the project

--- SalonKrasotyApp/Program.cs
+++ SalonKrasotyApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SalonKrasotyApp.ModelEF;
 
 namespace SalonKrasotyApp
 {
@@ -11,9 +12,14 @@ namespace SalonKrasotyApp
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        public static BeautySalonDB db = new BeautySalonDB();
         [STAThread]
         static void Main()
         {
+            if (db.Database.Exists() == false)
+            {
+                MessageBox.Show("Требуемая база данных не найдена!");
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainFrm());
