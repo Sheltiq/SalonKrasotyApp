f1171e6432cf7af0e30cdb21451495a26f4d0d62
Sheltiq <sheltiqq@gmail.com>
2025-05-11 17:48:01 +0500

    added search, sorting and filtering functionality

--- SalonKrasotyApp/MainFrm.Designer.cs
+++ SalonKrasotyApp/MainFrm.Designer.cs
@@ -28,8 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
             this.label1 = new System.Windows.Forms.Label();
+            this.productDataGridView = new System.Windows.Forms.DataGridView();
+            this.SearchTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SortCmb = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DownChk = new System.Windows.Forms.CheckBox();
+            this.FiltrCmb = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.RangeLbl = new System.Windows.Forms.Label();
+            this.manufacturerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.manufacturerIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.costDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.isActiveDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.productDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.manufacturerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -42,18 +64,202 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Товары салона красоты";
             // 
+            // productDataGridView
+            // 
+            this.productDataGridView.AllowUserToAddRows = false;
+            this.productDataGridView.AllowUserToDeleteRows = false;
+            this.productDataGridView.AutoGenerateColumns = false;
+            this.productDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.titleDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.manufacturerIDDataGridViewTextBoxColumn,
+            this.costDataGridViewTextBoxColumn,
+            this.isActiveDataGridViewCheckBoxColumn});
+            this.productDataGridView.DataSource = this.productBindingSource;
+            this.productDataGridView.Location = new System.Drawing.Point(10, 94);
+            this.productDataGridView.Name = "productDataGridView";
+            this.productDataGridView.ReadOnly = true;
+            this.productDataGridView.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.productDataGridView.RowTemplate.Height = 40;
+            this.productDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.productDataGridView.Size = new System.Drawing.Size(763, 379);
+            this.productDataGridView.TabIndex = 1;
+            this.productDataGridView.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.productDataGridView_CellFormatting);
+            // 
+            // SearchTxt
+            // 
+            this.SearchTxt.Location = new System.Drawing.Point(57, 68);
+            this.SearchTxt.Name = "SearchTxt";
+            this.SearchTxt.Size = new System.Drawing.Size(139, 23);
+            this.SearchTxt.TabIndex = 2;
+            this.SearchTxt.TextChanged += new System.EventHandler(this.SearchTxt_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Поиск";
+            // 
+            // SortCmb
+            // 
+            this.SortCmb.FormattingEnabled = true;
+            this.SortCmb.Items.AddRange(new object[] {
+            "Без сортировки",
+            "Название",
+            "Стоимость"});
+            this.SortCmb.Location = new System.Drawing.Point(285, 68);
+            this.SortCmb.Name = "SortCmb";
+            this.SortCmb.Size = new System.Drawing.Size(138, 24);
+            this.SortCmb.TabIndex = 4;
+            this.SortCmb.SelectedIndexChanged += new System.EventHandler(this.SortCmb_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(282, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Сортировка";
+            // 
+            // DownChk
+            // 
+            this.DownChk.AutoSize = true;
+            this.DownChk.Location = new System.Drawing.Point(431, 68);
+            this.DownChk.Name = "DownChk";
+            this.DownChk.Size = new System.Drawing.Size(91, 21);
+            this.DownChk.TabIndex = 6;
+            this.DownChk.Text = "убывание";
+            this.DownChk.UseVisualStyleBackColor = true;
+            this.DownChk.CheckedChanged += new System.EventHandler(this.DownChk_CheckedChanged);
+            // 
+            // FiltrCmb
+            // 
+            this.FiltrCmb.FormattingEnabled = true;
+            this.FiltrCmb.Items.AddRange(new object[] {
+            "Все типы"});
+            this.FiltrCmb.Location = new System.Drawing.Point(556, 68);
+            this.FiltrCmb.Name = "FiltrCmb";
+            this.FiltrCmb.Size = new System.Drawing.Size(157, 24);
+            this.FiltrCmb.TabIndex = 7;
+            this.FiltrCmb.SelectedIndexChanged += new System.EventHandler(this.FiltrCmb_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(553, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Фильтрация";
+            // 
+            // RangeLbl
+            // 
+            this.RangeLbl.AutoSize = true;
+            this.RangeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RangeLbl.Location = new System.Drawing.Point(239, 476);
+            this.RangeLbl.Name = "RangeLbl";
+            this.RangeLbl.Size = new System.Drawing.Size(124, 20);
+            this.RangeLbl.TabIndex = 9;
+            this.RangeLbl.Text = "Всего товаров:";
+            // 
+            // manufacturerBindingSource
+            // 
+            this.manufacturerBindingSource.DataSource = typeof(SalonKrasotyApp.ModelEF.Manufacturer);
+            // 
+            // productBindingSource
+            // 
+            this.productBindingSource.DataSource = typeof(SalonKrasotyApp.ModelEF.Product);
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.iDDataGridViewTextBoxColumn.Width = 46;
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Название";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            this.titleDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Описание";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // manufacturerIDDataGridViewTextBoxColumn
+            // 
+            this.manufacturerIDDataGridViewTextBoxColumn.DataPropertyName = "ManufacturerID";
+            this.manufacturerIDDataGridViewTextBoxColumn.DataSource = this.manufacturerBindingSource;
+            this.manufacturerIDDataGridViewTextBoxColumn.DisplayMember = "Name";
+            this.manufacturerIDDataGridViewTextBoxColumn.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.Nothing;
+            this.manufacturerIDDataGridViewTextBoxColumn.HeaderText = "Производитель";
+            this.manufacturerIDDataGridViewTextBoxColumn.Name = "manufacturerIDDataGridViewTextBoxColumn";
+            this.manufacturerIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.manufacturerIDDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.manufacturerIDDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.manufacturerIDDataGridViewTextBoxColumn.ValueMember = "ID";
+            this.manufacturerIDDataGridViewTextBoxColumn.Width = 135;
+            // 
+            // costDataGridViewTextBoxColumn
+            // 
+            this.costDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.costDataGridViewTextBoxColumn.DataPropertyName = "Cost";
+            dataGridViewCellStyle1.Format = "C2";
+            dataGridViewCellStyle1.NullValue = null;
+            this.costDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle1;
+            this.costDataGridViewTextBoxColumn.HeaderText = "Стоимость";
+            this.costDataGridViewTextBoxColumn.Name = "costDataGridViewTextBoxColumn";
+            this.costDataGridViewTextBoxColumn.ReadOnly = true;
+            this.costDataGridViewTextBoxColumn.Width = 103;
+            // 
+            // isActiveDataGridViewCheckBoxColumn
+            // 
+            this.isActiveDataGridViewCheckBoxColumn.DataPropertyName = "IsActive";
+            this.isActiveDataGridViewCheckBoxColumn.HeaderText = "IsActive";
+            this.isActiveDataGridViewCheckBoxColumn.Name = "isActiveDataGridViewCheckBoxColumn";
+            this.isActiveDataGridViewCheckBoxColumn.ReadOnly = true;
+            this.isActiveDataGridViewCheckBoxColumn.Visible = false;
+            // 
             // MainFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.RangeLbl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.FiltrCmb);
+            this.Controls.Add(this.DownChk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SortCmb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SearchTxt);
+            this.Controls.Add(this.productDataGridView);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainFrm";
             this.Text = "Информационная система «Салон красоты»‎ ";
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.productDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.manufacturerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -62,6 +268,23 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView productDataGridView;
+        private System.Windows.Forms.BindingSource productBindingSource;
+        private System.Windows.Forms.BindingSource manufacturerBindingSource;
+        private System.Windows.Forms.TextBox SearchTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox SortCmb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox DownChk;
+        private System.Windows.Forms.ComboBox FiltrCmb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label RangeLbl;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn manufacturerIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn costDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn isActiveDataGridViewCheckBoxColumn;
     }
 }
 
--- SalonKrasotyApp/MainFrm.cs
+++ SalonKrasotyApp/MainFrm.cs
@@ -7,14 +7,106 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SalonKrasotyApp.ModelEF;
 
 namespace SalonKrasotyApp
 {
     public partial class MainFrm : Form
     {
+        List<Product> lstFormatData = new List<Product>();
         public MainFrm()
         {
             InitializeComponent();
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            SortCmb.SelectedIndex = 0;
+            List<string> lstFiltr = Program.db.Manufacturer.Select(a => a.Name).OrderBy(s => s).ToList();
+            lstFiltr.Insert(0, "Все производители");
+            FiltrCmb.DataSource = lstFiltr;
+            FiltrCmb.SelectedIndex = 0;
+            manufacturerBindingSource.DataSource = Program.db.Manufacturer.ToList();
+            productBindingSource.DataSource = Program.db.Product.ToList();
+        }
+
+        private void produtcDataGridView_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+
+        }
+
+        private void productDataGridView_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.RowIndex == 0) return;
+
+            if (productDataGridView[5, e.RowIndex].Value == null)
+                return;
+
+            bool isactive = (bool)productDataGridView[5, e.RowIndex].Value;
+            if (isactive == false) { e.CellStyle.BackColor = Color.LightGray;}
+        }
+
+        string search = "";
+        string sort = "Без сортировки";
+        string fltr = "Все производители";
+
+        public void Podgotovka()
+        {
+            lstFormatData = Program.db.Product.ToList();
+            if (fltr != "Все производители")
+            {
+                lstFormatData = lstFormatData.Where(p => (p.Manufacturer.Name == fltr)).ToList();
+            }
+            if(search != "")
+            {
+                lstFormatData = lstFormatData.Where(p => p.Title.Contains(search)).ToList();
+                if (lstFormatData.Count == 0)
+                {
+                    MessageBox.Show("Строка " + search + " нигде не найдена!");
+                }
+            }
+            if (sort != "Без сортировки")
+            {
+                if(sort == "Название")
+                {
+                    if(!DownChk.Checked)
+                        lstFormatData = lstFormatData.OrderBy(p => p.Title).ToList();
+                    else
+                        lstFormatData = lstFormatData.OrderByDescending(p => p.Title).ToList();
+                }
+                if (sort == "Стоимость")
+                {
+                    if (!DownChk.Checked)
+                        lstFormatData = lstFormatData.OrderBy(p => p.Cost).ToList();
+                    else
+                        lstFormatData = lstFormatData.OrderByDescending(p => p.Cost).ToList();
+                }
+            }
+            RangeLbl.Text = $"Всего товаров: {lstFormatData.Count}";
+            productBindingSource.DataSource = lstFormatData;
+        }
+
+        private void FiltrCmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fltr = FiltrCmb.Text;
+            Podgotovka();
+        }
+
+        private void SortCmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sort = SortCmb.Text;
+            Podgotovka();
+        }
+
+        private void DownChk_CheckedChanged(object sender, EventArgs e)
+        {
+            Podgotovka();
+        }
+
+        private void SearchTxt_TextChanged(object sender, EventArgs e)
+        {
+            search = SearchTxt.Text;
+            Podgotovka();
         }
     }
 }
